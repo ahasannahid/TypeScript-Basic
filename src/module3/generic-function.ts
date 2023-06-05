c042e87d9b859115ceae00a7f8bfecf76a3d1ef2
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-05 12:50:07 +0600

    generic function touple

--- src/module3/generic-function.ts
+++ src/module3/generic-function.ts
@@ -3,11 +3,36 @@ const createArray=(param:string):string[]=>{
     return [param];
 }
 
-// generic arrow function
+// generic arrow function(single parameter T)
 
 const createArray1=<T>(param:T):T[]=>{
     return [param];
 }
 
+// generic arrow function(double parameter or touple)
+const createArray2=<X, Y>(param1:X, param2:Y):[X, Y]=>{
+    return [param1, param2];
+}
+
 const result1 = createArray('Banglaesh')
 const result2 = createArray1<string>('true')
+const result3 = createArray1<boolean>(true)
+const result4 = createArray1<object>({name:'Nahid'}) //not recommended
+const result5 = createArray1<{name:string}>({name:'Nahid'})
+
+// use type
+type Name = {name:string}
+const result6 = createArray1<Name>({name:'Nahid'})
+
+// use interface
+interface IName{
+    name:string
+}
+const result7 = createArray1<IName>({name:'Nahid'})
+
+// Touple
+
+const result8 = createArray2<string, string>('Bangladesh', 'I love BD')
+
+const result9 = createArray2<boolean, string[]>(true, ['USA'])
+const result10 = createArray2<boolean, Array<string>>(true, ['USA'])
